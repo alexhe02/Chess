c5f798824030be2a35964a457a108665391327f4
MasterProgrammer7354 <achowdavarapu@stu.naperville203.org>
2019-05-01 14:20:40 -0500

    Made pieces class and started pawn class

---
+++ Pawns.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * Abhinav Chowdavarapu and Alex He
+ * 5/1/19
+ */
+public class Pawns extends Pieces
+{
+    private int enPassantdamage;
+    public Pawns()
+    {
+        super(3,1,1,1);
+        enPassantdamage = 2;
+    }
+}
---
+++ Pieces.java
@@ -0,0 +1,67 @@
+
+
+/**
+ * Abhinav Chowdavarapu and Alex He
+ * 5/1/19
+ */
+public class Pieces
+{
+    protected int levelcost;
+    protected int level;
+    protected int experience;
+    protected int health;
+    protected int damage;
+    protected int attack;
+    protected int experienceondeath;
+    protected boolean blocking;
+    protected int numblocks;
+    protected boolean dead;
+    public Pieces(int levelcost, int health, int attack, int experienceondeath)
+    {
+        //stores the information that the subclasses will give it
+        level = 1;
+        experience = 0;
+        this.levelcost = levelcost;
+        this.health = health;
+        this.damage = 0;
+        this.attack = attack;
+        this.experienceondeath = experienceondeath;
+        blocking = false;
+        numblocks = 0;
+        dead = false;
+    }
+    
+    public void attack(Pieces other)
+    {
+        //deals damage if not blocking and removes block if blocking
+        if (!blocking)
+        {
+            other.damage += attack;
+        }
+        else
+        {
+            blocking = false;
+        }
+    }
+    
+    public void block()
+    {
+        //blocks and adds one to numblocks if numblocks < 6
+        if (numblocks < 6)
+        {
+            blocking = true;
+            numblocks += 1;
+        }
+    }
+    
+    public void die(Pieces other)
+    {
+        //checks if dead
+        //if true, dies and gives experienceondeath to the piece it was fighting
+        if (damage >= health)
+        {
+            this.dead = true;
+            other.experience += this.experienceondeath;
+        }
+    }
+}
