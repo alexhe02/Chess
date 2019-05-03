72fd90a909c0fd63bddb0fc5791e21c2cb1a0c60
MasterProgrammer7354 <achowdavarapu@stu.naperville203.org>
2019-05-03 11:18:27 -0500

    Started coding attack and block sequencing with special moves

---
+++ King.java
@@ -0,0 +1,53 @@
+
+
+/**
+ * Abhinav Chowdavarapu and Alex He
+ * 5/3/19
+ */
+public class King extends Pieces
+{
+    private double damageReduction;
+    private int tempattack;
+    public King()
+    {
+        super(16,3,1,2);
+        damageReduction = 0.1;
+        tempattack = attack;
+    }
+    
+    public void block()
+    {
+        super.block();
+    }
+    
+    public void charge()
+    {
+        tempattack *= 2;
+    }
+    
+    public void attack(Pieces other)
+    {
+        if (other.blocking)
+        {
+            tempattack = attack;
+            other.blocking = false;
+        }
+        else
+        {
+            other.damage += tempattack;
+            tempattack = attack;
+        }
+    }
+    
+    public void levelUp()
+    {
+        while (experience >= levelcost)
+        {
+            experience -= levelcost;
+            level += 1;
+            attack += 1;
+            health += 1;
+            damageReduction += 0.05;
+        }
+    }
+}
--- Queen.java
+++ Queen.java
@@ -2,10 +2,36 @@
 
 /**
  * Abhinav Chowdavarapu and Alex He
- * 5/2/19
+ * 5/3/19
  */
 public class Queen extends Pieces
 {
-    private int defendPercent;
+    private double defendPercent;
     public Queen()
+    {
+        super(5,4,4,1);
+        defendPercent = 0.1;
+    }
+    
+    public void attack(Pieces other)
+    {
+        super.attack(other);
+    }
+    
+    public void block()
+    {
+        super.block();
+    }
+    
+    public void levelUp()
+    {
+        while (experience >= levelcost)
+        {
+            experience -= levelcost;
+            level += 1;
+            attack += 1;
+            health += 1;
+            defendPercent += 0.05;
+        }
+    }
 }
