4ba3576125659beccb5feb60e914381ef988a5ca
MasterProgrammer7354 <achowdavarapu@stu.naperville203.org>
2019-05-14 13:33:44 -0500

    fixed things

--- Board.java
+++ Board.java
@@ -13,41 +13,29 @@ import javax.imageio.ImageIO;
 public class Board extends Applet implements MouseListener {
     public static Pieces[][] boardstate = new Pieces[8][8];
     
-    public static void main(String[] args)
+    public void init()
     {
         for (int i = 0; i < boardstate.length; i++)
         {
             boardstate[1][i] = new Pawns();
             boardstate[6][i] = new Pawns();
         }
-        boardstate[1][0] = new Knight();
-        boardstate[1][7] = new Knight();
-        boardstate[6][0] = new Knight();
-        boardstate[6][7] = new Knight();
+        boardstate[0][2] = new Knight();
+        boardstate[7][2] = new Knight();
+        boardstate[0][5] = new Knight();
+        boardstate[7][5] = new Knight();
         boardstate[0][0] = new Rook();
-        boardstate[0][7] = new Rook();
         boardstate[7][0] = new Rook();
+        boardstate[0][7] = new Rook();
         boardstate[7][7] = new Rook();
-<<<<<<< HEAD
-        boardstate[2][0] = new Bishop();
-        
-        boardstate[2][7] = new Bishop();
-        boardstate[5][2] = new Bishop();
-        boardstate[5][7] = new Bishop();
-        boardstate[3][0] = new King();
-        boardstate[3][7] = new King();
-        boardstate[4][0] = new Queen();
-        boardstate[4][7] = new Queen();
-=======
-        boardstate[0][2] = new Bishop();
-        boardstate[7][2] = new Bishop();
-        boardstate[0][5] = new Bishop();
-        boardstate[7][5] = new Bishop();
+        boardstate[0][1] = new Bishop();    
+        boardstate[7][1] = new Bishop();
+        boardstate[0][6] = new Bishop();
+        boardstate[7][6] = new Bishop();
         boardstate[0][3] = new King();
         boardstate[7][3] = new King();
         boardstate[0][4] = new Queen();
         boardstate[7][4] = new Queen();
->>>>>>> parent of b3b6f44... Fixed some issues with the images
     }
     
     public void mouseClicked(MouseEvent m)
@@ -83,23 +71,19 @@ public class Board extends Applet implements MouseListener {
                 y = col * 60; 
                 if ((row % 2 == 0) == (col % 2 == 0)) 
                 {
-                    if (boardstate[row][col] == null)
-                    {
+                    //if (boardstate[row][col] == null)
+                    //{
                         Color Black = new Color(13,13,13);
                         g.setColor(Black);
-                    }
-                    else
-                    {
-                        
-                    }
+                    //}
                 }
                 else
                 {
-                    if(boardstate[row][col] == null)
-                    {
+                    //if(boardstate[row][col] == null)
+                    //{
                         g.setColor(Color.GRAY); 
                         g.fillRect(x, y, 60, 60);
-                    }
+                    //}
                 }
             } 
         } 
